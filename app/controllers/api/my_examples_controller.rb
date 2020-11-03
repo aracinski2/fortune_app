fdfcbf64c37bafc1758b45190441d9b39208756d
Alex Racinski <aracinski2@gmail.com>
2020-11-02 21:09:39 -0600

    made the first feature which gives random fortune

--- app/controllers/api/my_examples_controller.rb
+++ app/controllers/api/my_examples_controller.rb
@@ -1,6 +1,9 @@
 class Api::MyExamplesController < ApplicationController
   
   def random_fortune_action
+    fortunes = ["You will be wealthy", "You will be happy", "You will be healthy"]
+    @fortune = fortunes.sample
+    
     render "feature_1.json.jb"
   end
 
